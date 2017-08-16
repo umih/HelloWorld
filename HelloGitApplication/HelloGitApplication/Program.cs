541bc4339075f180447ff7cdbc35ec36e5c9f348
UivarosM <UivarosM@AT-UIVAROSM>
2017-08-16 13:50:45 +0200

    Test commit

---
+++ HelloGitApplication/HelloGitApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGitApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
